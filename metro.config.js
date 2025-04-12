1d3307d023c6bc87d9da7739aeed3f7aadb7737a
Vinayak Soni <vinayaksoni106@gmail.com>
2025-04-12 20:23:49 +0530

    Initial commit

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const customConfig = mergeConfig(defaultConfig, {});
+
+module.exports = wrapWithReanimatedMetroConfig(customConfig);
